10995f48f688f9ba90f111ff51264e8647d253e5
SatoShun <shun.sato1@gmail.com>
2015-09-26 05:18:27 +0000

    Load application.cfg

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
+import os
+
 from flask import Flask
 
-from config import config
+from config import config, basedir
 
 
 def create_app(config_name):
     app = Flask(__name__)
+
     app.config.from_object(config[config_name])
+    cfg_path = os.path.join(basedir, 'application.cfg')
+    if os.path.isfile(cfg_path) and config_name != 'testing':
+        app.config.from_pyfile(cfg_path)
     config[config_name].init_app(app)
 
     # WWW
